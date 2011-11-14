7d7ac0794b4bd2bd274fdfdaf81c67a231736d07
Marcin <devnull@localhost>
2011-11-14 11:14:32 -0500

    prep for open/edit file auto-upload

--- src/com/CH_co/cryptx/SHA256.java
+++ src/com/CH_co/cryptx/SHA256.java
@@ -32,6 +32,8 @@ import java.security.*;
  */
 public final class SHA256 extends HashBase implements Cloneable {
 
+  public static final String name = "SHA-256";
+
   private static final int[] k = {
        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -58,7 +60,7 @@ public final class SHA256 extends HashBase implements Cloneable {
 
   /** No argument constructor. */
   public SHA256() {
-    super("SHA-256", 32, BLOCK_SIZE);
+    super(name, 32, BLOCK_SIZE);
   }
 
   /**
