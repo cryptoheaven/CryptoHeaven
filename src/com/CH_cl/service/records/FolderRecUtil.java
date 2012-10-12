6f26441168fa345393397e7b620766549feaf085
Marcin <Marcin@Marcin7-PC>
2012-10-12 14:32:30 -0400

    utility methods to find folders fetched and invalidated

--- src/com/CH_cl/service/records/FolderRecUtil.java
+++ src/com/CH_cl/service/records/FolderRecUtil.java
@@ -1,70 +1,87 @@
 /*
- * Copyright 2001-2012 by CryptoHeaven Corp.,
- * Mississauga, Ontario, Canada.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of CryptoHeaven Corp. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with CryptoHeaven Corp.
- */
+* Copyright 2001-2012 by CryptoHeaven Corp.,
+* Mississauga, Ontario, Canada.
+* All rights reserved.
+*
+* This software is the confidential and proprietary information
+* of CryptoHeaven Corp. ("Confidential Information").  You
+* shall not disclose such Confidential Information and shall use
+* it only in accordance with the terms of the license agreement
+* you entered into with CryptoHeaven Corp.
+*/
 
 package com.CH_cl.service.records;
 
 import java.util.*;
 
 /** 
- * <b>Copyright</b> &copy; 2001-2012
- * <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
- * CryptoHeaven Corp.
- * </a><br>All rights reserved.<p>
- *
- * Class Description:
- *
- *
- * Class Details:
- *
- *
- * <b>$Revision: 1.6 $</b>
- * @author  Marcin Kurzawa
- * @version
- */
+* <b>Copyright</b> &copy; 2001-2012
+* <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
+* CryptoHeaven Corp.
+* </a><br>All rights reserved.<p>
+*
+* Class Description:
+*
+*
+* Class Details:
+*
+*
+* <b>$Revision: 1.6 $</b>
+* @author  Marcin Kurzawa
+* @version
+*/
 public class FolderRecUtil extends Object {
 
-  // When folder contents is fetched, lets keep these marks here.
-  private static final Hashtable fldFetchRequestsIssuedHT = new Hashtable();
+  // When folder contents is fetched, lets keep these folderIDs here.
+  private static final HashSet fldFetchRequestsIssuedHT = new HashSet();
 
   // When folder view is invalidated, lets keep these marks here.
   private static final Hashtable fldViewInvalidatedHT = new Hashtable();
 
-
-  public static void markFolderFetchRequestIssued(Long folderId) {
-    fldFetchRequestsIssuedHT.put(folderId, Boolean.TRUE);
+  public static synchronized List getFoldersFetched() {
+    ArrayList list = new ArrayList();
+    Iterator iter = fldFetchRequestsIssuedHT.iterator();
+    while (iter.hasNext()) {
+      Long folderId = (Long) iter.next();
+      list.add(folderId);
+    }
+    return list;
   }
-  public static boolean wasFolderFetchRequestIssued(Long folderId) {
-    Boolean b = Boolean.FALSE;
-    if (folderId != null)
-      b = (Boolean) fldFetchRequestsIssuedHT.get(folderId);
-    return b != null ? b.booleanValue() : false;
+
+  public static synchronized List getFoldersFetchedAndInvalidated() {
+    ArrayList list = new ArrayList();
+    Iterator iter = fldFetchRequestsIssuedHT.iterator();
+    while (iter.hasNext()) {
+      Long folderId = (Long) iter.next();
+      if (wasFolderViewInvalidated(folderId))
+        list.add(folderId);
+    }
+    return list;
   }
 
-  public static void markFolderViewInvalidated(Long folderId, boolean isInvalidated) {
+  public static synchronized void markFolderFetchRequestIssued(Long folderId) {
+    fldFetchRequestsIssuedHT.add(folderId);
+  }
+  public static synchronized boolean wasFolderFetchRequestIssued(Long folderId) {
+    return folderId != null && fldFetchRequestsIssuedHT.contains(folderId);
+  }
+
+  public static synchronized void markFolderViewInvalidated(Long folderId, boolean isInvalidated) {
     fldViewInvalidatedHT.put(folderId, Boolean.valueOf(isInvalidated));
   }
-  public static boolean wasFolderViewInvalidated(Long folderId) {
+  public static synchronized boolean wasFolderViewInvalidated(Long folderId) {
     Boolean b = Boolean.FALSE;
     if (folderId != null)
       b = (Boolean) fldViewInvalidatedHT.get(folderId);
     return b != null ? b.booleanValue() : false;
   }
 
-  public static void clear() {
+  public static synchronized void clear() {
     fldFetchRequestsIssuedHT.clear();
     fldViewInvalidatedHT.clear();
   }
 
-  public static void clearFetchedIDs() {
+  public static synchronized void clearFetchedIDs() {
     fldFetchRequestsIssuedHT.clear();
   }
 
