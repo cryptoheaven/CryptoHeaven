a502e758f1ba1ef29ddec614e5db3a43023653ce
Marcin <Marcin@Marcin7-PC>
2013-01-04 12:27:10 -0500

    cache utility that converts to familiar should check for null address book to prevent exception

--- src/com/CH_cl/service/cache/CacheEmlUtils.java
+++ src/com/CH_cl/service/cache/CacheEmlUtils.java
@@ -1,5 +1,5 @@
 /*
-* Copyright 2001-2012 by CryptoHeaven Corp.,
+* Copyright 2001-2013 by CryptoHeaven Corp.,
 * Mississauga, Ontario, Canada.
 * All rights reserved.
 *
@@ -19,7 +19,7 @@ import com.CH_co.trace.Trace;
 import java.util.HashSet;
 
 /** 
-* <b>Copyright</b> &copy; 2001-2012
+* <b>Copyright</b> &copy; 2001-2013
 * <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
 * CryptoHeaven Corp.
 * </a><br>All rights reserved.<p>
@@ -56,30 +56,33 @@ public class CacheEmlUtils extends Object {
       UserRecord uRec = cache.getUserRecord();
       // First look in the primary Address Book
       FolderRecord addrBook = cache.getFolderRecord(uRec.addrFolderId);
-      Long addrBookId = addrBook.folderId;
-      MsgLinkRecord[] addrLinks = cache.getMsgLinkRecordsForMsgs(addrIDs);
-      if (addrLinks != null && addrLinks.length > 0) {
-        // See which of those links is in our main address book
-        for (int i=0; i<addrLinks.length; i++) {
-          MsgLinkRecord addrLink = addrLinks[i];
-          if (addrLink.ownerObjType.shortValue() == Record.RECORD_TYPE_FOLDER && addrLink.ownerObjId.equals(addrBookId)) {
-            addrRecord = cache.getMsgDataRecord(addrLink.msgId);
-            break;
-          }
-        }
-        if (addrRecord == null) {
-          // If nothing found, look in all Address Books
-          // Prep all address book lookup table
-          FolderRecord[] addrBooks = cache.getFolderRecords(new FolderFilter(FolderRecord.ADDRESS_FOLDER));
-          HashSet addrBookIDs = new HashSet();
-          for (int i=0; i<addrBooks.length; i++)
-            addrBookIDs.add(addrBooks[i].folderId);
-          // Look for matching links
+      // AddressBook may not be fetched yet... skip
+      if (addrBook != null) {
+        Long addrBookId = addrBook.folderId;
+        MsgLinkRecord[] addrLinks = cache.getMsgLinkRecordsForMsgs(addrIDs);
+        if (addrLinks != null && addrLinks.length > 0) {
+          // See which of those links is in our main address book
           for (int i=0; i<addrLinks.length; i++) {
             MsgLinkRecord addrLink = addrLinks[i];
-            if (addrLink.ownerObjType.shortValue() == Record.RECORD_TYPE_FOLDER && addrBookIDs.contains(addrLink.ownerObjId)) {
+            if (addrLink.ownerObjType.shortValue() == Record.RECORD_TYPE_FOLDER && addrLink.ownerObjId.equals(addrBookId)) {
               addrRecord = cache.getMsgDataRecord(addrLink.msgId);
               break;
+            }
+          }
+          if (addrRecord == null) {
+            // If nothing found, look in all Address Books
+            // Prep all address book lookup table
+            FolderRecord[] addrBooks = cache.getFolderRecords(new FolderFilter(FolderRecord.ADDRESS_FOLDER));
+            HashSet addrBookIDs = new HashSet();
+            for (int i=0; i<addrBooks.length; i++)
+              addrBookIDs.add(addrBooks[i].folderId);
+            // Look for matching links
+            for (int i=0; i<addrLinks.length; i++) {
+              MsgLinkRecord addrLink = addrLinks[i];
+              if (addrLink.ownerObjType.shortValue() == Record.RECORD_TYPE_FOLDER && addrBookIDs.contains(addrLink.ownerObjId)) {
+                addrRecord = cache.getMsgDataRecord(addrLink.msgId);
+                break;
+              }
             }
           }
         }
