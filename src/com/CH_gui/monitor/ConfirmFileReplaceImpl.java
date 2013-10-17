a9dd4a0e9cfc2f2550256b6452e12839acf31cde
Marcin <Marcin@Marcin7-PC>
2013-10-17 16:37:54 -0400

    double confirmation dialog when file replace was canceled...

--- src/com/CH_gui/monitor/ConfirmFileReplaceImpl.java
+++ src/com/CH_gui/monitor/ConfirmFileReplaceImpl.java
@@ -1,14 +1,14 @@
 /*
- * Copyright 2001-2013 by CryptoHeaven Corp.,
- * Mississauga, Ontario, Canada.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of CryptoHeaven Corp. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with CryptoHeaven Corp.
- */
+* Copyright 2001-2013 by CryptoHeaven Corp.,
+* Mississauga, Ontario, Canada.
+* All rights reserved.
+*
+* This software is the confidential and proprietary information
+* of CryptoHeaven Corp. ("Confidential Information").  You
+* shall not disclose such Confidential Information and shall use
+* it only in accordance with the terms of the license agreement
+* you entered into with CryptoHeaven Corp.
+*/
 
 package com.CH_gui.monitor;
 
@@ -28,21 +28,21 @@ import javax.swing.border.*;
 import javax.swing.*;
 
 /** 
- * <b>Copyright</b> &copy; 2001-2013
- * <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
- * CryptoHeaven Corp.
- * </a><br>All rights reserved.<p>
- *
- * Class Description:
- *
- *
- * Class Details:
- *
- *
- * <b>$Revision: 1.14 $</b>
- * @author  Marcin Kurzawa
- * @version
- */
+* <b>Copyright</b> &copy; 2001-2013
+* <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
+* CryptoHeaven Corp.
+* </a><br>All rights reserved.<p>
+*
+* Class Description:
+*
+*
+* Class Details:
+*
+*
+* <b>$Revision: 1.14 $</b>
+* @author  Marcin Kurzawa
+* @version
+*/
 public class ConfirmFileReplaceImpl extends GeneralDialog implements ConfirmFileReplaceI {
 
   private static final int DEFAULT_YES_INDEX = 1;
