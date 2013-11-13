07a34ffff900c53f05bfb5b0b28f577f6ce76d98
Marcin <Marcin@Marcin7-PC>
2013-11-13 17:34:42 -0500

    minor - test commit ver 1.0

--- src/com/CH_gui/action/AbstractActionTraced.java
+++ src/com/CH_gui/action/AbstractActionTraced.java
@@ -1,33 +1,31 @@
-/*
- * Copyright 2001-2013 by CryptoHeaven Corp.,
- * Mississauga, Ontario, Canada.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of CryptoHeaven Corp. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with CryptoHeaven Corp.
- */
-
+/**
+* Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
+*
+* This software is the confidential and proprietary information
+* of CryptoHeaven Corp. ("Confidential Information").  You
+* shall not disclose such Confidential Information and shall use
+* it only in accordance with the terms of the license agreement
+* you entered into with CryptoHeaven Corp.
+*/
 package com.CH_gui.action;
 
-import com.CH_co.util.MyUncaughtExceptionHandlerOps;
 import com.CH_co.trace.Trace;
+import com.CH_co.util.MyUncaughtExceptionHandlerOps;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Icon;
 
 /**
- * <b>Copyright</b> &copy; 2001-2013
- * <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
- * CryptoHeaven Corp.
- * </a><br>All rights reserved.<p>
- *
- *
- * @author  Marcin Kurzawa
- * @version
- */
+* Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
+*
+* Class Description:
+*
+* Class Details:
+*
+* <b>$Revision: 1.0 $</b>
+*
+* @author  Marcin Kurzawa
+*/
 public abstract class AbstractActionTraced extends AbstractAction {
 
   public AbstractActionTraced() {
