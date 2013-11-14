7fdd7ee0958e1f3dd66563d1263716af664ced74
Marcin <Marcin@Marcin7-PC>
2013-11-13 19:23:35 -0500

    minor -revision test

--- src/com/CH_co/service/ops/EmailSendingOps.java
+++ src/com/CH_co/service/ops/EmailSendingOps.java
@@ -1,7 +1,5 @@
-/*
- * Copyright 2001-2013 by CryptoHeaven Corp.,
- * Mississauga, Ontario, Canada.
- * All rights reserved.
+/**
+ * Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
  *
  * This software is the confidential and proprietary information
  * of CryptoHeaven Corp. ("Confidential Information").  You
@@ -9,15 +7,22 @@
  * it only in accordance with the terms of the license agreement
  * you entered into with CryptoHeaven Corp.
  */
-
 package com.CH_co.service.ops;
 
 import com.CH_co.service.records.EmailRecord;
 import com.CH_co.util.URLs;
 
 /**
+ * Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
  *
- * @author Marcin
+ * Class Description:
+ *
+ * Class Details:
+ *
+ * <b>$Revision: 1.0 $</b>
+ * $LastChangedRevision$
+ *
+ * @author  Marcin Kurzawa
  */
 public class EmailSendingOps {
 
