fdc58be67e838c8a32e696f963f2f522be22b1a2
mkurzawa <kurzawa@aprava.com>
2014-03-29 21:41:56 -0400

    build 832 Fix exception due to missing language string.

--- src/com/CH_co/util/GlobalProperties.java
+++ src/com/CH_co/util/GlobalProperties.java
@@ -326,8 +326,9 @@ public class GlobalProperties extends Object {
   //           Inviting users using full email address uses personal part for naming of your Contact object.
   // build 828 Move BrowserLauncher to the gui side of API
   // build 830 Catch more common exception as per bug reports.
+  // build 832 Fix exception due to missing language string.
 
-  public static final short PROGRAM_BUILD_NUMBER = 830;  // even
+  public static final short PROGRAM_BUILD_NUMBER = 832;  // even
   public static final boolean IS_BETA = false;
 
   // These final values are used in other places during compilation... keep them final!
